7a1cbb5be92e102f1d60aaae47fcd66d23e2254b
kazunoriboy <matunorigmnw@gmail.com>
2022-02-13 18:17:23 +0900

    送信するファイルのmimetypeを設定

--- work/simplepost/main.go
+++ work/simplepost/main.go
@@ -7,13 +7,17 @@ import (
     "mime/multipart"
     "net/http"
     "os"
+    "net/textproto"
 )
 
 func main() {
     var buffer bytes.Buffer
     writer := multipart.NewWriter(&buffer)
     writer.WriteField("name", "Michael Jackson")
-    fileWriter, err := writer.CreateFormFile("thumbnail", "photo.jpg")
+    part := make(textproto.MIMEHeader)
+    part.Set("Content-Type", "image/jpeg")
+    part.Set("Content-Disposition", `form-data; name="thumbnail"; filename="photo.jpg"`)
+    fileWriter, err := writer.CreatePart(part)
     if err != nil {
         panic(err)
     }
@@ -21,7 +25,6 @@ func main() {
     if err != nil {
         panic(err)
     }
-    defer readFile.Close()
     io.Copy(fileWriter, readFile)
     writer.Close()
     resp, err := http.Post("http://localhost:18888", writer.FormDataContentType(), &buffer)
