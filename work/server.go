1e93847ed99f11f032698be455564bcb2c744112
kazunoriboy <matunorigmnw@gmail.com>
2023-05-07 15:56:30 +0900

    プロトコルのアップグレードを行うためのサーバー側の実装

--- work/server.go
+++ work/server.go
@@ -3,10 +3,12 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
+	"time"
 
 	"github.com/k0kubun/pp"
 )
@@ -36,6 +38,42 @@ func handlerDigest(w http.ResponseWriter, r *http.Request) {
 	} else {
 
 		fmt.Fprintf(w, "<html><body>secret page</body></html>\n")
+	}
+}
+
+func handlerUpgrade(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Connection") != "Upgrade" || r.Header.Get("Upgrade") != "MyProtocol" {
+		w.WriteHeader(400)
+		return
+	}
+	fmt.Println("Upgrade to MyProtocol")
+
+	hijacker := w.(http.Hijacker)
+	conn, readWriter, err := hijacker.Hijack()
+	if err != nil {
+		panic(err)
+		return
+	}
+	defer conn.Close()
+
+	response := http.Response{
+		StatusCode: 101,
+		Header:     make(http.Header),
+	}
+	response.Header.Set("Upgrade", "MyProtocol")
+	response.Header.Set("Connection", "Upgrade")
+	response.Write(conn)
+
+	for i := 1; i <= 10; i++ {
+		fmt.Fprintf(readWriter, "%d\n", i)
+		fmt.Println("->", i)
+		readWriter.Flush()
+		recv, err := readWriter.ReadBytes('\n')
+		if err != io.EOF {
+			break
+		}
+		fmt.Printf("<- %s", string(recv))
+		time.Sleep(500 * time.Millisecond)
 	}
 }
 
